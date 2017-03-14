5eb131442f5b10892a1c69678d733e82ee186583
meron93 <meronyemane18@gmail.com>
2017-03-13 18:11:02 -0700

    spotify api implemented

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
 $(document).ready(function() {
 
-  /**
- * This is an example of a basic node.js script that performs
- * the Client Credentials oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
- */
-
-//var request = require('request'); // "Request" library
-
   var client_id = 'f508191a9bf4407ca3d35a6c7f8eb795'; // Your client id
   var client_secret = 'e8dd51b0aea94a168594cb1a938cb888'; // Your secret
-
-  // // find template and compile it
-  // var templateSource = document.getElementById('results-template').innerHTML,
-  //     template = Handlebars.compile(templateSource),
-  //     resultsPlaceholder = document.getElementById('results'),
-  //     playingCssClass = 'playing',
-  //     audioObject = null;
 
   var fetchTracks = function (albumId, callback) {
       $.ajax({
@@ -32,66 +14,48 @@ $(document).ready(function() {
 
   var searchAlbums = function (query) {
     console.log("Search Called")
+    var html = "";
       $.ajax({
           url: 'https://api.spotify.com/v1/search',
           data: {
               q: "genre:" + query, 
-              type: "artist" 
+              type: "artist",
+              limit: "5" 
           },
           //success: function (response) {
           //    console.log(response);
           //}
           success: function(response) {
             console.log(response)
-            for (i = 0; i < response["artists"]["items"].length; i++) {
+            var htmlTracker = 0 
+            for (i = 0; i < 5; i++) {
+              console.log(i)
               console.log(response["artists"]["items"][i]["name"])
               $.ajax({
                 url: 'https://api.spotify.com/v1/search',
                 data: {
                   q: "artist:" + response["artists"]["items"][i]["name"],
-                  type: "track"
+                  type: "track",
+                  limit: "1"
                 },
                 success: function(track) {
+                  console.log(track)
                   console.log(track["tracks"]["items"][0]["name"]);
+                  var id = track["tracks"]["items"][0]["id"];
+                  html += '<iframe src="https://embed.spotify.com/?uri=spotify:track:' + id + '" frameborder="0" allowtransparency="true"></iframe>'             
+                  htmlTracker++
+                  if (htmlTracker === 5) {
+                    console.log(html);
+                    $(".container").html(html);
+                  };
                 }
               })
             }
-            
-
-
-            console.log(response.length)
-            //for (i = 0; i < response.length; i++) {
-              //console.log("fh");
-              //console.log(response[i][id]);
-            //};
+          $(".container").html(html); 
           }, 
       });
   };
 
-
-  // results.addEventListener('click', function(e) {
-  //     var target = e.target;
-  //     if (target !== null && target.classList.contains('cover')) {
-  //         if (target.classList.contains(playingCssClass)) {
-  //             audioObject.pause();
-  //         } else {
-  //             if (audioObject) {
-  //                 audioObject.pause();
-  //             }
-  //             fetchTracks(target.getAttribute('data-album-id'), function(data) {            
-  //                 audioObject = new Audio(data.tracks.items[0].preview_url);
-  //                 audioObject.play();
-  //                 target.classList.add(playingCssClass);
-  //                 audioObject.addEventListener('ended', function() {
-  //                     target.classList.remove(playingCssClass);
-  //                 });
-  //                 audioObject.addEventListener('pause', function() {
-  //                     target.classList.remove(playingCssClass);
-  //                });
-  //             });
-  //         }
-  //     }
-  // });
 
 
   $('#myform').submit(function(e) {
