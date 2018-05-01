1d63d52ab8aea4243082672fb1f464c8063c4ad9
meron93 <meronyemane18@gmail.com>
2018-05-01 15:44:17 -0700

    Implemented request.js

--- public/app.js
+++ public/app.js
@@ -78,87 +78,87 @@ const genres = ["Rap", "House", "Rock", "Pop", "Country", "Classical", "Latin", 
 // });
 
 
-// var searchGenre = function (query) {
-//   console.log("query", query)
-//   var html = "";
-//     $.ajax({
-//       url: 'https://accounts.spotify.com/api/token',
-//       type: 'POST',
-//       // contentType: 'application/x-www-form-urlencoded',
-//       json: true,
-//       form: {
-//         grant_type: 'client_credentials'
-//       },
-//       headers: {
-//         'Access-Control-Allow-Origin': '*',
-//         'Authorization': 'Basic ' + client_id + ':' + client_secret
-//       },
-//       success: function(error, response, body) {
-//         var token = body.access_token;
-//         $.ajax({
-//             url: 'https://api.spotify.com/v1/search',
-//             type: 'GET',
-//             data: {
-//                 q: "genre:" + '"' + query + '"', 
-//                 type: "artist",
-//                 limit: "20" 
-//             },
-//             json: true,
-//             headers: {
-//               'Access-Control-Allow-Origin': '*',
-//               'Authorization': 'Bearer ' + token
-//             },
-//             success: function(error, response, body) {
-//               console.log("error", error)
-//               console.log("response", response)
-//               console.log("body", body)
-//               if (response["artists"]["items"].length > 0) {
-//                 var htmlTracker = 0
-//                 artist_lst = []
-//                 html += '<p class="display-genre">Top tracks for: ' + query + '</p>'  
-//                 for (i = 0; i < 5; i++) {
-//                   while (true) {
-//                     var artist_num = Math.floor(Math.random() * 20.0);
-//                     if (!artist_lst.includes(artist_num)) {
-//                       break
-//                     };
-//                   };
-//                   artist_lst.push(artist_num);
-//                   $.ajax({
-//                     url: 'https://api.spotify.com/v1/search',
-//                     data: {
-//                       q: "artist:" + response["artists"]["items"][artist_num]["name"],
-//                       type: "track",
-//                       limit: "5"
-//                     },
-//                     type: 'GET',
-//                     json: true,
-//                     success: function(track) {
-//                       var track_num = Math.floor(Math.random() * 5.0);
-//                       var id = track["tracks"]["items"][track_num]["id"];
-//                       html += '<iframe class="song" src="https://embed.spotify.com/?uri=spotify:track:' + id + '" frameborder="0" allowtransparency="true"></iframe>'             
-//                       htmlTracker++
-//                       if (htmlTracker === 5) {
-//                         $(".container").html(html);
-//                       };
-//                     }
-//                   })
-//                 }
-//               } else {
-//                 html = "";
-//                 html += '<p class="error">hmm...looks like the genre didnt return any results. Try searching for another genre, or disKover new music with our drop down list.</p>';
-//                 $(".container").html(html);
-//               }
-//             $(".container").html(html); 
-//             }, 
-//         });
-//       }
-//   });
-// };
+var searchGenre = function (query) {
+  console.log("query", query)
+  var html = "";
+    $.ajax({
+      url: 'https://accounts.spotify.com/api/token',
+      type: 'POST',
+      // contentType: 'application/x-www-form-urlencoded',
+      json: true,
+      form: {
+        grant_type: 'client_credentials'
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': 'Basic ' + client_id + ':' + client_secret
+      },
+      success: function(error, response, body) {
+        var token = body.access_token;
+        $.ajax({
+            url: 'https://api.spotify.com/v1/search',
+            type: 'GET',
+            data: {
+                q: "genre:" + '"' + query + '"', 
+                type: "artist",
+                limit: "20" 
+            },
+            json: true,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Authorization': 'Bearer ' + token
+            },
+            success: function(error, response, body) {
+              console.log("error", error)
+              console.log("response", response)
+              console.log("body", body)
+              if (response["artists"]["items"].length > 0) {
+                var htmlTracker = 0
+                artist_lst = []
+                html += '<p class="display-genre">Top tracks for: ' + query + '</p>'  
+                for (i = 0; i < 5; i++) {
+                  while (true) {
+                    var artist_num = Math.floor(Math.random() * 20.0);
+                    if (!artist_lst.includes(artist_num)) {
+                      break
+                    };
+                  };
+                  artist_lst.push(artist_num);
+                  $.ajax({
+                    url: 'https://api.spotify.com/v1/search',
+                    data: {
+                      q: "artist:" + response["artists"]["items"][artist_num]["name"],
+                      type: "track",
+                      limit: "5"
+                    },
+                    type: 'GET',
+                    json: true,
+                    success: function(track) {
+                      var track_num = Math.floor(Math.random() * 5.0);
+                      var id = track["tracks"]["items"][track_num]["id"];
+                      html += '<iframe class="song" src="https://embed.spotify.com/?uri=spotify:track:' + id + '" frameborder="0" allowtransparency="true"></iframe>'             
+                      htmlTracker++
+                      if (htmlTracker === 5) {
+                        $(".container").html(html);
+                      };
+                    }
+                  })
+                }
+              } else {
+                html = "";
+                html += '<p class="error">hmm...looks like the genre didnt return any results. Try searching for another genre, or disKover new music with our drop down list.</p>';
+                $(".container").html(html);
+              }
+            $(".container").html(html); 
+            }, 
+        });
+      }
+  });
+};
 
 var renderStarterPage = function() {
   var html = '<div class="col-3">'  
-  html += '<p class="opening-msg">Welcome to disKover, an app that allows you to explore the currently most popular songs in any given genre!</p>'
+  html += '<p class="opening-msg">Welcome to disKov, an app that allows you to explore the currently most popular songs in any given genre!</p>'
   html += '<form id="opening-button">'; 
   html += '<button class="start-button" type="submit">Start Exploring!</button></form>'
   html += '</div>'
@@ -185,10 +185,13 @@ $(document).on("submit", "#opening-button", function(event) {
     $('.search').val("");
     //searchGenre(search);
     $.ajax({
-      url: "search/genresearch",
+      data: JSON.stringify(search),
+      contentType: "application/json",
+      url: "/genresearch",
       type: "GET"
-    }).done(function(data, status) {
-      console.log("hello")
+    }).done(function(html) {
+      console.log(html)
+      $(".container").html(html);
     })
   });
 })
--- server.js
+++ server.js
@@ -3,23 +3,28 @@
 const express = require('express');
 const app = express();
 const request = require('request'); 
+const jsonParser = require('body-parser').json();
 
 const client_id = 'f508191a9bf4407ca3d35a6c7f8eb795';
-const client_secret = '44c1ba6d88714eeca37e42eddaf463b';
-
+const client_secret = '44c1ba6d88714eeca37e42eddaf463b1';
+//const cors = require('cors')
 app.use(express.static('public'));
+app.use(jsonParser);
 
-app.listen(process.env.PORT || 8080, () => console.log("listening"));
-const genreRouter = express.Router();
-
+//app.listen(process.env.PORT || 8080, () => console.log("listening"));
+//const genreRouter = express.Router();
+// app.use(cors({
+//   origin: 'http://localhost:8080'
+// }));
 // your application requests authorization
 
-
-genreRouter.get('/genresearch', (req, res) => {
-  console.log('inside genreRouter')
+app.get('/genresearch', (req, res) => {
+  var html = "";
+  const genre = Object.keys(req.query)[0].replace(/['"]+/g, '')
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     headers: {
+      'Access-Control-Allow-Origin': '*',
       'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
     },
     form: {
@@ -28,27 +33,74 @@ genreRouter.get('/genresearch', (req, res) => {
     json: true
   };
 
-  request.post(authOptions, function(error, response, body, genre) {
-    console.log("response", response)
-    console.log("body", body)
+  request.post(authOptions, function(error, response, body) {
+    console.log("genre", genre)
     if (!error && response.statusCode === 200) {
 
       // use the access token to access the Spotify Web API
       var token = body.access_token;
       var options = {
-        url: 'https://api.spotify.com/v1/',
-        data: "genre:" + '"' + query + '"',
+        url: 'https://api.spotify.com/v1/search',
+        qs: {
+          q: "genre:" + '"' + genre + '"',
+          type: "artist",
+          limit: "20"
+        },
         headers: {
+          'Access-Control-Allow-Origin': '*',
           'Authorization': 'Bearer ' + token
         },
         json: true
       };
       request.get(options, function(error, response, body) {
-        console.log(body);
+        // console.log("MADDEE IITTTTTT");
+        // console.log("response", response)
+        // console.log("body", body["artists"]["items"])
+        if (body["artists"]["items"].length > 0) {
+          var htmlTracker = 0
+          var artist_lst = []
+          html += '<p class="display-genre">Top tracks for: ' + genre + '</p>'  
+          for (var i = 0; i < 5; i++) {
+            while (true) {
+              var artist_num = Math.floor(Math.random() * 20.0);
+              if (!artist_lst.includes(artist_num)) {
+                break
+              };
+            };
+            artist_lst.push(artist_num);
+            var trackOptions = {
+              url: 'https://api.spotify.com/v1/search',
+              qs: {
+                q: "artist:" + body["artists"]["items"][artist_num]["name"],
+                type: "track",
+                limit: "5"
+              },        
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': 'Bearer ' + token
+              },
+              json: true
+            };
+            request.get(trackOptions, function(error, response, body) {
+              var track_num = Math.floor(Math.random() * 5.0);
+              console.log("body", body)
+              var id = body["tracks"]["items"][track_num]["id"];
+              html += '<iframe class="song" src="https://open.spotify.com/embed?uri=spotify:track:' + id + '" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'             
+              htmlTracker++
+              if (htmlTracker === 5) {
+                return res.status(200).json(html)
+              };
+            });
+          }
+        } else {
+          html = "";
+          html += '<p class="error">hmm...looks like the genre didnt return any results. Try searching for another genre, or disKover new music with our drop down list.</p>';
+          return res.status(200).json(html)
+        }
       });
     }
   });
 });
-app.use('/search/', genreRouter);
-
-module.exports = {app, genreRouter};
+// app.use('/search/', genreRouter);
+app.listen(process.env.PORT || 8080, () => console.log("listening"));
+module.exports = {app};
